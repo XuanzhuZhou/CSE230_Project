defef7e51744fce147db054876048b5927cde773
zhouxuanzhu <zhouxuanzhu@zhouxuanzhudeMacBook-Pro.local>
2021-12-08 11:19:56 -0800

    remove extra comment

--- src/Maze.hs
+++ src/Maze.hs
@@ -4,23 +4,11 @@
 {-# OPTIONS_GHC -Wno-unused-matches #-}
 {-# OPTIONS_GHC -Wno-incomplete-patterns #-}
 {-# OPTIONS_GHC -Wno-overlapping-patterns #-}
--- module Maze
---   ( initGame
---   , step
---   , turn
---   , Game(..)
---   , Direction(..)
---   , dead, food, score, snake
---   , height, width
---   ) where
 module Maze(
   initGame
   , moves
   , p1_kill
   , p2_kill
-  -- , turn1
-  , turn1
-  , turn2
   , Game(..)
   , Direction(..)
   , dead, paused, player1, player2, score1, score2, bullets, solid, normal, grass
@@ -38,7 +26,6 @@ import Data.Sequence (Seq(..), (<|))
 import qualified Data.Sequence as S
 import Linear.V2 (V2(..), _x, _y)
 import System.Random (Random(..), newStdGen)
-import Control.Monad.Error (MonadError(throwError))
 
 -- Types
 
@@ -49,7 +36,6 @@ data Game = Game
   , _player2 :: Coord    -- ^ coordinate of player 2 
   , _score1  :: Int          -- ^ score of player 1
   , _score2  :: Int          -- ^ score of player 2
-  -- , _bullet  :: Coord        -- ^ list of bullets locations
   , _bullets :: [Coord] -- ^ list of bullets locations
   , _solid   :: [Coord]      -- ^ list of solid blocks
   , _normal  :: [Coord]      -- ^ list of normal blocks
@@ -57,8 +43,6 @@ data Game = Game
   } deriving (Show)
 
 type Coord = V2 Int
-
-type Player = Seq Coord
 
 data Stream a = a :| Stream a
   deriving (Eq,Show)
@@ -109,82 +93,12 @@ moves player dirc g = do
     else
       g & player2 %~ (\(V2 a b) -> V2 new_x new_y)
 
-
--- moveCoord :: Int -> Direction-> Game -> Game
--- moveCoord num d g
---   | num==1 =
---   if d == North then g & player1 %~ (\(V2 a b) -> V2 a ((b+1) `mod` height))
---   else if d == South then g & player1 %~ (\(V2 a b) -> V2 a ((b-1) `mod` height))
---   else if d == West then g & player1 %~ (\(V2 a b) -> V2 ((a-1) `mod` width) b)
---   else g & player1 %~ (\(V2 a b) -> V2 ((a+1) `mod` width) b)
---   | d == North = g & player2 %~ (\(V2 a b) -> V2 a ((b+1) `mod` height))
---   | d == South = g & player2 %~ (\(V2 a b) -> V2 a ((b-1) `mod` height))
---   | d == West = g & player2 %~ (\(V2 a b) -> V2 ((a-1) `mod` width) b)
---   | otherwise = g & player2 %~ (\(V2 a b) -> V2 ((a+1) `mod` width) b)
-
--- moves  :: Int -> Direction -> Game -> Game
--- moves num d g = do
---   let (V2 x y) = if num == 1 then g ^. player1 else g ^. player2
---   let bullet_x bullet_y
---         | d == North = x y+1
---         | d == South = x y-1
---         | d == West = (x-1) y
---         | otherwise = (x+1) y
---   if blockExists x y g d then
---     g
---   else
---     if bulletExists x y g d then
---       moveCoord num d (addScore bullet_x bullet_y num d g)
---     else moveCoord num d
-
--- moves num South g = do
---   let (V2 x y) = if num == 1 then g ^. player1 else g ^. player2
---   let curr_bullet = V2 x (y-1)
---   if blockExists x y g South then
---     g
---   else
---     if bulletExists g South then
---       addScore num South g & player1 %~ (\(V2 a b) -> V2 a ((b-1) `mod` height)) & bullets %~ delete curr_bullet
---     else g & player1 %~ (\(V2 a b) -> V2 a ((b-1) `mod` height))
-
--- moves num West g = do
---   let (V2 x y) = if num == 1 then g ^. player1 else g ^. player2
---   let curr_bullet = V2 (x-1) y
---   if blockExists x y g West then
---     g
---   else
---     if bulletExists g West then
---       addScore num West g & player1 %~ (\(V2 a b) -> V2 ((a-1) `mod` width) b) & bullets %~ delete curr_bullet
---     else g & player1 %~ (\(V2 a b) -> V2 ((a-1) `mod` width) b)
-
--- moves num East g = do
---   let (V2 x y) = if num == 1 then g ^. player1 else g ^. player2
---   let curr_bullet = V2 (x+1) y
---   if blockExists x y g East then
---     g
-  -- else
-    -- if bulletExists g East then
-    --   addScore num East g & player1 %~ (\(V2 a b) -> V2 ((a+1) `mod` width) b) & bullets %~ delete curr_bullet
-    -- else g & player1 %~ (\(V2 a b) -> V2 ((a+1) `mod` width) b)
-
-
-
 blockExists :: Int -> Int -> Game -> Direction -> Bool
 blockExists xm ym g d = case d of
-  North -> V2 (xm-1) ym+1 `elem` g ^. normal -- !!!
-  South -> V2 (xm+1) ym-1 `elem` g ^. normal -- !!!
-  West ->  V2 (xm-1) ym `elem` g ^. normal -- !!!
-  East ->  V2 (xm+1) ym `elem` g ^. normal -- !!!
---   V2 (xm-1) ym+1 `elem` g ^. normal
-
--- blockExists xm ym g South =
---   V2 (xm+1) ym-1 `elem` g ^. normal
-
--- blockExists xm ym g East =
---   V2 (xm+1) ym `elem` g ^. normal
-
--- blockExists xm ym g West =
---   V2 (xm-1) ym `elem` g ^. normal
+  North -> V2 (xm-1) ym+1 `elem` g ^. normal
+  South -> V2 (xm+1) ym-1 `elem` g ^. normal
+  West ->  V2 (xm-1) ym `elem` g ^. normal
+  East ->  V2 (xm+1) ym `elem` g ^. normal
 
 addScore  :: Int -> Game -> Game
 addScore num g =
@@ -197,60 +111,10 @@ delete deleted list = [x | x <- list, x /= deleted]
 
 bulletExists :: Int -> Int -> Game -> Direction -> Bool
 bulletExists xm ym g d = case d of
-  North -> V2 (xm-1) ym+1 `elem` g ^. bullets -- !!!
-  South -> V2 (xm+1) ym-1 `elem` g ^. bullets -- !!!
-  West ->  V2 (xm-1) ym `elem` g ^. bullets -- !!!
-  East ->  V2 (xm+1) ym `elem` g ^. bullets -- !!!
-
--- bulletExists xm ym g North =
---   V2 (xm-1) ym+1 `elem` g ^. bullets -- !!!
-
--- bulletExists g South =
---   V2 (xm+1) ym-1 `elem` g ^. bullets -- !!!
-
--- bulletExists g West =
---   V2 (xm-1) ym `elem` g ^. bullets -- !!!
-
--- bulletExists g East =
---   V2 (xm+1) ym `elem` g ^. bullets -- !!!
--- | Initialize a paused game with random location
-
-turn2 South g = do
-  if blockExists2 g South then
-    g
-  else
-    g & player2 %~ (\(V2 a b) -> V2 a ((b-1) `mod` height))
-
-turn2 West g = do
-  if blockExists2 g West then
-    g
-  else
-    g & player2 %~ (\(V2 a b) -> V2 ((a-1) `mod` width) b)
-  
-turn2 East g = do
-  if blockExists2 g East then
-    g
-  else
-    g & player2 %~ (\(V2 a b) -> V2 ((a+1) `mod` width) b)
-
-turn2 _ _   = error "Player1 can't be empty!"
-
-blockExists2 :: Game -> Direction -> Bool
-blockExists2 g North = do
-  let (V2 xm ym) = g ^. player2
-  (V2 xm ym+1) `elem` (g ^. normal)
-
-blockExists2 g South = do
-  let (V2 xm ym) = g ^. player2
-  (V2 xm ym-1) `elem` (g ^. normal)
-
-blockExists2 g East = do
-  let (V2 xm ym) = g ^. player2
-  V2 (xm+1) ym `elem` (g ^. normal)
-
-blockExists2 g West = do
-  let (V2 xm ym) = g ^. player2
-  V2 (xm-1) ym `elem` (g ^. normal)
+  North -> V2 (xm-1) ym+1 `elem` g ^. bullets
+  South -> V2 (xm+1) ym-1 `elem` g ^. bullets
+  West ->  V2 (xm-1) ym `elem` g ^. bullets
+  East ->  V2 (xm+1) ym `elem` g ^. bullets
 
 -- | Define some random blocks for initialization
 -- randShape1 x y = [V2 x y, V2 x y+1, V2 x y+2, V2 (x+1) y, V2 (x+2) y]
@@ -272,8 +136,6 @@ initBullets = [V2 30 15, V2 28 25, V2 10 19, V2 5 30]
 
 initGame :: IO Game
 initGame = do
-  -- (f :| fs) <-
-  --   fromList . randomRs (V2 0 0, V2 (width - 1) (height - 1)) <$> newStdGen
   let xm = width `div` 5
       ym = height `div` 5
       g  = Game{
@@ -283,7 +145,6 @@ initGame = do
         , _player2 = V2 (xm+10) (ym+10)              -- ^ coordinate of player 2 
         , _score1  =  0                            -- ^ score of player 1
         , _score2  =  0                            -- ^ score of player 2  
-        -- , _bullet   = f                            -- ^ coordinate of current bullet
         , _bullets = initBullets                   -- ^ list of bullets locations
         , _solid   = initSolid                     -- ^ list of solid blocks
         , _normal  = initNormal                    -- ^ list of normal blocks
@@ -295,9 +156,6 @@ initState :: State Game ()
 initState = do
   s <- get
   put s
-
-fromList :: [a] -> Stream a
-fromList = foldr (:|) (error "Streams must be infinite")
 
 ------------------------------------------------------------
 ----------------- kill (using bullets) ---------------------
