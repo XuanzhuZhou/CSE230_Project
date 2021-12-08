d2e99461fdd621efb9d16bc30134dcd4b01b90b6
zhouxuanzhu <zhouxuanzhu@zhouxuanzhudeMacBook-Pro.local>
2021-12-08 14:57:03 -0800

    add test package

--- tests/Test.hs
+++ tests/Test.hs
@@ -1,9 +1,15 @@
 module Main where 
 
 import System.Exit
+import Test.QuickCheck
 
 main :: IO ()
 main = do 
   putStrLn "\nRunning my tests... "
+  quickCheck prop_isAllDigit
   putStrLn "\nDone Testing"
   exitWith ExitSuccess 
+
+-- test functions
+prop_isAllDigit :: Int -> Bool
+prop_isAllDigit val = val == val
